136bb1b074aaf29a3961bb31e2dc3aaefbd4ba6a
zhuqiyang <qiqitao320@qq.com>
2026-01-27 00:16:30 +0800

    edit files

--- roles/addons/files/addons/6.install_nvidia-device-plugin.sh
+++ roles/addons/files/addons/6.install_nvidia-device-plugin.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+cat > runtimeclass.yaml <<EOF
+apiVersion: node.k8s.io/v1
+kind: RuntimeClass
+metadata:
+  name: nvidia
+handler: nvidia
+EOF
+
+kubectl apply -f runtimeclass.yaml
+
+
+
 tar -xf nvidia-device-plugin-0.18.0.tgz
 
 cd nvidia-device-plugin
